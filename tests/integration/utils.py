6ab4a4a0dcd30cf69d73f597d1ee44711c3ed7a9
Miroslav Hanak <miroslav.hanak@nic.cz>
2020-07-18 10:42:15 +0200

    tests - integration - framework - add utils

---
+++ tests/integration/utils.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+import random
+import string
+
+
+def gen_rand_byte_str(len):
+    """ Generates random byte string of given length from 0-255 values.
+        returns bytearray """
+    return bytearray(random.choice(list(range(0, 256))) for n in range(len))
+
+
+def gen_rand_ascii_print_byte_str(len):
+    """ Generates random string from printable characters only (32-126) of given len.
+        returns bytearray """
+    return bytearray(random.choice(list(range(32, 127))) for n in range(len))
+
+
+def gen_rand_str(len):
+    """ Generates random string of given length from all 0-127 ASCII char set.
+        returns string """
+    return ''.join([chr(_) for _ in range(128)])
+
+
+def gen_rand_printable_str(len):
+    """ Generates random string from only ASCII printable chars 32-126 of given length.
+        returns string """
+    s = ''.join(random.choices(string.printable, k=len))
+    # replace is here because a sequence of \r was generated and it caused error while parsing command in a minipot
+    s = s.replace("\r", "/")
+    # for sake of safety replace other escape sequences as well
+    s = s.replace("\n", "/")
+    s = s.replace("\a", "/")
+    s = s.replace("\b", "/")
+    s = s.replace("\f", "/")
+    return s 
+
+
+def send_to_sock(sock, data):
+    """ Sends given data to given socket.
+        sock - socket
+        data - bytes
+        returns number of bytes sent to the socket - int """
+    return sock.sendall(data)
+
+
+def recv_from_sock(sock):
+    # TODO check socket settings
+    """ Receives 128 bytes from socket 128 bytes.
+        If no data are available rerurn empty string.
+        returns bytes"""
+    try:
+        # TODO change receive buffer size here
+        data = sock.recv(128)
+    except BlockingIOError:
+        return b''
+    else:
+        return data
+
+
+def str_cmp(str1, str2):
+    """ Checks if str1 is equal to str2. If not exception is raised. """
+    if str1 != str2:
+        raise Exception('str_cmp - strings does not match: ')
+
+
+def get_ip_addr(sock):
+    """ Get peer IP address from a socket.
+        returns string """
+    ip_addr, port = sock.getpeername()
+    return ip_addr
