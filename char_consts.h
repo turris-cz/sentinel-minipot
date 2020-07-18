f2d3f43fa9428dd8a688884f637e73a5ca8c064e
Miroslav Hanak <miroslav.hanak@nic.cz>
2020-07-18 21:45:35 +0200

    Add characters constants

---
+++ char_consts.h
@@ -0,0 +1,34 @@
+/*
+ *  Turris:Sentinel Minipot - password Honeypot
+ *  Copyright (C) 2019-2020 CZ.NIC z.s.p.o. (https://www.nic.cz/)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef __SENTINEL_MINIPOT_CHAR_CONSTS_H__
+#define __SENTINEL_MINIPOT_CHAR_CONSTS_H__
+
+#define HT '\t'
+#define LF '\n'
+#define VT '\v'
+#define FF '\f'
+#define CR '\r'
+#define SP ' '
+
+#define STAR '*'
+#define DOUBLE_DOT ':'
+#define COMMA ','
+#define SEMICOLON ';'
+
+#endif /* __SENTINEL_MINIPOT_CHAR_CONSTS_H__ */
