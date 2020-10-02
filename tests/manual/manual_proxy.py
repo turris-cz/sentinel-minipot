45c4185d3705c6ce57be8650bc8a039a5023b1cf
Miroslav Hanak <miroslav.hanak@nic.cz>
2020-10-02 16:16:11 +0200

    manual tests: refactoring
    To improve code quality

--- tests/manual/manual_proxy.py
+++ tests/manual/manual_proxy.py
@@ -4,17 +4,17 @@ import msgpack
 import sys
 
 def parse_msg(msg):
-	type = str(msg[0], encoding="UTF-8")
-	payload = msgpack.unpackb(msg[1], raw=True)
-	return type, payload
+    type = str(msg[0], encoding="UTF-8")
+    payload = msgpack.unpackb(msg[1], raw=True)
+    return type, payload
 
 
 if __name__ == "__main__":
-	socket_path = str(sys.argv[1])
-	print("sentinel test proxy running on socket: ", socket_path)
-	with zmq.Context() as context, context.socket(zmq.PULL) as zmq_sock:
-		zmq_sock.bind(socket_path)
-		while True:
-			zmq_msg = zmq_sock.recv_multipart()
-			msg_type, msg_payload = parse_msg(zmq_msg)
-			print("{}: {}".format(msg_type, msg_payload))
+    socket_path = str(sys.argv[1])
+    print("manual proxy running on socket: ", socket_path)
+    with zmq.Context() as context, context.socket(zmq.PULL) as zmq_sock:
+        zmq_sock.bind(socket_path)
+        while True:
+            zmq_msg = zmq_sock.recv_multipart()
+            msg_type, msg_payload = parse_msg(zmq_msg)
+            print("{}: {}".format(msg_type, msg_payload))
