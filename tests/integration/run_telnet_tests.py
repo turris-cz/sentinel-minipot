a6e700a383442182831fffc3ce1ac215bc3fd792
Miroslav Hanak <miroslav.hanak@nic.cz>
2020-11-11 16:46:04 +0100

    telnet: integration tests: refactoring

--- tests/integration/run_telnet_tests.py
+++ tests/integration/run_telnet_tests.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
-from framework.test import Test
-import telnet
 import random
+
+
+from framework.test import Test
+import telnet as t
+
 
 if __name__ == "__main__":
     # TODO setup here
@@ -11,11 +14,14 @@ if __name__ == "__main__":
     host = '127.0.0.1'
     port = 9008
     log_dir = "logs/telnet"
-    # TODO run tests here
 
-    Test("login_test_1", log_dir, proxy_sock, [(telnet.login_test1, host, port )]).run()
-    Test("login_test_2", log_dir, proxy_sock, [(telnet.login_test2, host, port )]).run()
-    Test("login_test_3", log_dir, proxy_sock, [(telnet.login_test3, host, port )]).run()
-    Test("brute_force_test_1", log_dir, proxy_sock, [(telnet.bruteforce_test, host, port )]).run()
-    Test("multiple_brute_force_test_1", log_dir, proxy_sock, [(telnet.bruteforce_test, host, port ),
-    (telnet.bruteforce_test, host, port ), (telnet.bruteforce_test, host, port ), (telnet.bruteforce_test, host, port ), (telnet.bruteforce_test, host, port )]).run()
+    # TODO run tests here
+    Test("login_test_1", log_dir, proxy_sock, [(t.login_test1, host, port)]).run()
+    Test("login_test_2", log_dir, proxy_sock, [(t.login_test2, host, port)]).run()
+    Test("login_test_3", log_dir, proxy_sock, [(t.login_test3, host, port)]).run()
+    Test("login_test_4", log_dir, proxy_sock, [(t.login_test4, host, port)]).run()
+    Test("login_test_5", log_dir, proxy_sock, [(t.login_test5, host, port)]).run()
+    Test("brute_force_test_1", log_dir, proxy_sock, [(t.bruteforce_test, host, port)]).run()
+    Test("multiple_brute_force_test_1", log_dir, proxy_sock, [(t.bruteforce_test, host, port),
+         (t.bruteforce_test, host, port), (t.bruteforce_test, host, port),
+         (t.bruteforce_test, host, port), (t.bruteforce_test, host, port)]).run()
--- tests/integration/telnet.py
+++ tests/integration/telnet.py
@@ -1,171 +1,248 @@
-#!/usr/bin/env python3
+from random import choice, randint
 
-from framework.utils import *
 from framework.proxy import gen_proxy_report
+from framework.utils import get_ip_addr, recv_from_sock
 
 
-MAX_CONN_COUNT = 5
+MINIPOT_MAX_LINE_LEN = 1024
+MINIPOT_MAX_ATTEMPTS = 20
 
-MAX_LINE_LEN = 1024
-MAX_ATTEMPTS = 20
+MINIPOT_CONNECT_EV = b"connect"
+MINIPOT_LOGIN_EV = b"login"
+MINIPOT_TYPE_EV = b"telnet"
 
-CONNECT_EV = b"connect"
-LOGIN_EV = b"login"
-TYPE = b"telnet"
+MINIPOT_LOGIN_USER = b"username"
+MINIPOT_LOGIN_PASS = b"password"
 
-LOGIN_USER = b"username"
-LOGIN_PASS = b"password"
-
-ASK_FOR_USER = b"Username: \xff\xf9"
-ASK_FOR_PASSW = b"Password: \xff\xf9"
-PROTOCOL_ERR = b"Protocol error\r\n\xff\xf9"
-INCORR_LOGIN = b"Login incorrect\r\n\xff\xf9"
+MINIPOT_ASK_FOR_USER = b"Username: \xff\xf9"
+MINIPOT_ASK_FOR_PASSW = b"Password: \xff\xf9"
+MINIPOT_PROTOCOL_ERR = b"Protocol error\r\n\xff\xf9"
+MINIPOT_INCORR_LOGIN = b"Login incorrect\r\n\xff\xf9"
 
 
-def gen_connect_report(ip):
-    """ Generates proxy report connect message.
-        ip - string
-        returns dictionary """
-    return gen_proxy_report(TYPE, CONNECT_EV, ip, None)
+CRLF = b"\r\n"
 
 
-def gen_login_report(ip, user=b"", password=b""):
-    """ Generates proxy report login message.
-        ip - string
-        user -  bytes
-        password - bytes
-        returns dictionary"""
+def gen_connect_report(sock):
+    """ Generates proxy report connect message and returns it as dictionary.
+
+    Parameters:
+        sock: socket """
+    return gen_proxy_report(MINIPOT_TYPE_EV, MINIPOT_CONNECT_EV, get_ip_addr(sock))
+
+
+def gen_login_report(sock, user=b"", password=b""):
+    """ Generates proxy report login message and returns it as dictionary.
+
+    Parameters:
+        sock: socket
+        user: bytes
+        password: bytes """
     data = {
-        LOGIN_USER: user,
-        LOGIN_PASS: password,
+        MINIPOT_LOGIN_USER: user,
+        MINIPOT_LOGIN_PASS: password,
     }
-    return gen_proxy_report(TYPE, LOGIN_EV, ip, data)
+    return gen_proxy_report(MINIPOT_TYPE_EV, MINIPOT_LOGIN_EV, get_ip_addr(sock), data)
+
+
+def gen_username(len):
+    """ Returns byte string generated randomly from values 0-254 without 13.
+
+    Parameters:
+        len: int """
+    bytevals = list(range(255))
+    bytevals.remove(13)  # CR
+    return bytes([choice(bytevals) for _ in range(len)])
+
+
+def gen_password(len):
+    """ Returns byte string generated randomly from values 0-254 without 13.
+
+    Parameters:
+        len: int """
+    bytevals = list(range(255))
+    bytevals.remove(13)  # CR
+    return bytes([choice(bytevals) for _ in range(len)])
+
+
+################################################################################
+# tests
 
 
 def login_test1(server_sock):
-    ip_addr = get_ip_addr(server_sock)
-    reports = [gen_connect_report(ip_addr)]
+    """ Sends to Minipots:
+        username longer than MINIPOT_MAX_LINE_LEN
+        password longer than MINIPOT_MAX_LINE_LEN
+    Required response from Minipots':
+        MINIPOT_ASK_FOR_USER
+        MINIPOT_ASK_FOR_PASSW
+        MINIPOT_INCORR_LOGIN
+    Required Minipots generated Sentinel message:
+        connect
+        login
+    More description:
+
+    Parameters:
+        server_sock: socket
+    Returns list of dictionaries - generated proxy reports or assert
+    exception if communication with minipot went wrong. """
     response = recv_from_sock(server_sock)
-    # print(response)
-    assert response == ASK_FOR_USER
-
-    bytelist = list(range(33,126))
-
-    user = gen_rand_bytes(bytelist, 4090)
-
-    cmd = b"".join([user, b"\r\n"])
-    server_sock.sendall(cmd)
-
+    assert response == MINIPOT_ASK_FOR_USER
+    user = gen_username(4000)
+    server_sock.sendall(user + CRLF)
     response = recv_from_sock(server_sock)
-    # print(response)
-    assert response == ASK_FOR_PASSW
-
-    passw = gen_rand_bytes(bytelist, 4090)
-
-    cmd = b"".join([passw, b"\r\n"])
-    server_sock.sendall(cmd)
-
-    reports.append(gen_login_report(ip_addr, user[:MAX_LINE_LEN], passw[:MAX_LINE_LEN]))
-
+    assert response == MINIPOT_ASK_FOR_PASSW
+    passw = gen_password(4000)
+    server_sock.sendall(passw + CRLF)
     response = recv_from_sock(server_sock)
-    # print(response)
-    assert response == INCORR_LOGIN
-
-    return reports
+    assert response == MINIPOT_INCORR_LOGIN
+    return [gen_connect_report(server_sock),
+            gen_login_report(server_sock, user=user[:MINIPOT_MAX_LINE_LEN],
+            password=passw[:MINIPOT_MAX_LINE_LEN])]
 
 
 def login_test2(server_sock):
-    ip_addr = get_ip_addr(server_sock)
-    reports = [gen_connect_report(ip_addr)]
+    """ Sends to Minipots:
+        empty username - CRLF
+        empty password - CRLF
+    Required response from Minipots':
+        MINIPOT_ASK_FOR_USER
+        MINIPOT_ASK_FOR_PASSW
+        MINIPOT_INCORR_LOGIN
+    Required Minipots generated Sentinel message:
+        connect
+    More description:
+
+    Parameters:
+        server_sock: socket
+    Returns list of dictionaries - generated proxy reports or assert
+    exception if communication with minipot went wrong. """
     response = recv_from_sock(server_sock)
-    # print(response)
-    assert response == ASK_FOR_USER
-
-
-    cmd = b"\r\n"
-    server_sock.sendall(cmd)
-
+    assert response == MINIPOT_ASK_FOR_USER
+    server_sock.sendall(CRLF)
     response = recv_from_sock(server_sock)
-    # print(response)
-    assert response == ASK_FOR_PASSW
-
-    cmd = b"\r\n"
-    server_sock.sendall(cmd)
-
-    reports.append(gen_login_report(ip_addr))
-
+    assert response == MINIPOT_ASK_FOR_PASSW
+    server_sock.sendall(CRLF)
     response = recv_from_sock(server_sock)
-    # print(response)
-    assert response == INCORR_LOGIN
-
-    return reports
-
+    assert response == MINIPOT_INCORR_LOGIN
+    return [gen_connect_report(server_sock), gen_login_report(server_sock)]
 
 
 def login_test3(server_sock):
-    ip_addr = get_ip_addr(server_sock)
-    reports = [gen_connect_report(ip_addr)]
+    """ Sends to Minipots:
+        one byte username
+        one byte password
+    Required response from Minipots':
+        MINIPOT_ASK_FOR_USER
+        MINIPOT_ASK_FOR_PASSW
+        MINIPOT_INCORR_LOGIN
+    Required Minipots generated Sentinel message:
+        connect
+        login
+    More description:
+
+    Parameters:
+        server_sock: socket
+    Returns list of dictionaries - generated proxy reports or assert
+    exception if communication with minipot went wrong. """
     response = recv_from_sock(server_sock)
-    # print(response)
-    assert response == ASK_FOR_USER
-
-    bytelist = list(range(33,126))
-
-    user = gen_rand_bytes(bytelist, 1)
-
-    cmd = b"".join([user, b"\r\n"])
-    server_sock.sendall(cmd)
-
+    assert response == MINIPOT_ASK_FOR_USER
+    user = gen_username(1)
+    server_sock.sendall(user + CRLF)
     response = recv_from_sock(server_sock)
-    # print(response)
-    assert response == ASK_FOR_PASSW
-
-    passw = gen_rand_bytes(bytelist, 1)
-
-    cmd = b"".join([passw, b"\r\n"])
-    server_sock.sendall(cmd)
-
-    reports.append(gen_login_report(ip_addr, user, passw))
-
+    assert response == MINIPOT_ASK_FOR_PASSW
+    passw = gen_password(1)
+    server_sock.sendall(passw + CRLF)
     response = recv_from_sock(server_sock)
-    # print(response)
-    assert response == INCORR_LOGIN
+    assert response == MINIPOT_INCORR_LOGIN
+    return [gen_connect_report(server_sock),
+            gen_login_report(server_sock, user=user, password=passw)]
 
-    return reports
+
+def login_test4(server_sock):
+    """ Sends to Minipots:
+        random username
+        empty password
+    Required response from Minipots':
+        MINIPOT_ASK_FOR_USER
+        MINIPOT_ASK_FOR_PASSW
+        MINIPOT_INCORR_LOGIN
+    Required Minipots generated Sentinel message:
+        connect
+        login
+    More description:
+
+    Parameters:
+        server_sock: socket
+    Returns list of dictionaries - generated proxy reports or assert
+    exception if communication with minipot went wrong. """
+    response = recv_from_sock(server_sock)
+    assert response == MINIPOT_ASK_FOR_USER
+    server_sock.sendall(user + CRLF)
+    response = recv_from_sock(server_sock)
+    assert response == MINIPOT_ASK_FOR_PASSW
+    server_sock.sendall(CRLF)
+    response = recv_from_sock(server_sock)
+    assert response == MINIPOT_INCORR_LOGIN
+    return [gen_connect_report(server_sock), gen_login_report(server_sock, user=user)]
 
 
+def login_test5(server_sock):
+    """ Sends to Minipots:
+        empty username
+        random password
+    Required response from Minipots':
+        MINIPOT_ASK_FOR_USER
+        MINIPOT_ASK_FOR_PASSW
+        MINIPOT_INCORR_LOGIN
+    Required Minipots generated Sentinel message:
+        connect
+        login
+    More description:
+
+    Parameters:
+        server_sock: socket
+    Returns list of dictionaries - generated proxy reports or assert
+    exception if communication with minipot went wrong. """
+    response = recv_from_sock(server_sock)
+    assert response == MINIPOT_ASK_FOR_USER
+    server_sock.sendall(CRLF)
+    response = recv_from_sock(server_sock)
+    assert response == MINIPOT_ASK_FOR_PASSW
+    passw = gen_password(randint(1, MINIPOT_MAX_LINE_LEN))
+    server_sock.sendall(passw + CRLF)
+    response = recv_from_sock(server_sock)
+    assert response == MINIPOT_INCORR_LOGIN
+    return [gen_connect_report(server_sock), gen_login_report(server_sock, password=passw)]
 
 
 def bruteforce_test(server_sock):
-    ip_addr = get_ip_addr(server_sock)
-    reports = [gen_connect_report(ip_addr)]
+    """ Sends to Minipots:
+        MINIPOT_MAX_ATTEMPTS times usernames, passwords with random length
+    Required response from Minipots':
+        MINIPOT_ASK_FOR_USER
+        MINIPOT_ASK_FOR_PASSW
+        MINIPOT_INCORR_LOGIN
+    Required Minipots generated Sentinel message:
+        connect
+        login for each login attempt
+    More description:
 
-    for i in range(MAX_ATTEMPTS):
+    Parameters:
+        server_sock: socket
+    Returns list of dictionaries - generated proxy reports or assert
+    exception if communication with minipot went wrong. """
+    reports = [gen_connect_report(server_sock)]
+    for i in range(MINIPOT_MAX_ATTEMPTS):
         response = recv_from_sock(server_sock)
-        # print(response)
-        assert response == ASK_FOR_USER
-        bytelist = list(range(33,126))
-
-        user = gen_rand_bytes(bytelist, 4090)
-
-        cmd = b"".join([user, b"\r\n"])
-        server_sock.sendall(cmd)
-
+        assert response == MINIPOT_ASK_FOR_USER
+        user = gen_username(randint(0, MINIPOT_MAX_LINE_LEN))
+        server_sock.sendall(user + CRLF)
         response = recv_from_sock(server_sock)
-        # print(response)
-        assert response == ASK_FOR_PASSW
-
-        passw = gen_rand_bytes(bytelist, 4090)
-
-        cmd = b"".join([passw, b"\r\n"])
-        server_sock.sendall(cmd)
-
-        reports.append(gen_login_report(ip_addr, user[:1024], passw[:1024]))
-
+        assert response == MINIPOT_ASK_FOR_PASSW
+        passw = gen_password(randint(0, MINIPOT_MAX_LINE_LEN))
+        server_sock.sendall(passw + CRLF)
         response = recv_from_sock(server_sock)
-        print(response)
-        # print(INCORR_LOGIN)
-        # print("------")
-        assert response == INCORR_LOGIN
-
+        assert response == MINIPOT_INCORR_LOGIN
+        reports.append(gen_login_report(server_sock, user=user, password=passw))
     return reports
