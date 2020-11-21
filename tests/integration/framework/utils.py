0d7f737e830a0d181820b520a43414923ef4df52
Miroslav Hanak <miroslav.hanak@nic.cz>
2020-11-21 18:55:06 +0100

    integration tests: add utf-8 generators

--- tests/integration/framework/utils.py
+++ tests/integration/framework/utils.py
@@ -1,4 +1,4 @@
-import random
+import random as r
 import sys
 
 
@@ -25,4 +25,45 @@ def gen_rand_bytes_w10(len):
         len: int """
     bytevals = list(range(256))
     bytevals.remove(10)  # LF
-    return bytes([random.choice(bytevals) for _ in range(len)])
+    return bytes([r.choice(bytevals) for _ in range(len)])
+
+
+def gen_rand_utf8_char(len):
+    """ Returns randomly generated utf-8 char NOT containing NULL byte with
+    given length as bytes.
+
+    Parameters:
+        len: int
+            number of bytes for the char
+            Must be 1,2,3 or 4 otherwise Exception is returned. """
+    if len == 1:
+        ret = bytes([r.randint(0, 127)])
+    elif len == 2:
+        ret = bytes([r.randint(194, 223), r.randint(128, 191)])
+    elif len == 3:
+        ret = r.choice([bytes([224, r.randint(160, 191), r.randint(128, 191)]),
+                        bytes([r.randint(225, 236), r.randint(128, 191), r.randint(128, 191)]),
+                        bytes([237, r.randint(128, 159), r.randint(128, 191)]),
+                        bytes([r.randint(238, 239), r.randint(128, 191), r.randint(128, 191)])])
+    elif len == 4:
+        ret = r.choice([bytes([240, r.randint(144, 191),
+                               r.randint(128, 191), r.randint(128, 191)]),
+                        bytes([r.randint(241, 243), r.randint(128, 191),
+                               r.randint(128, 191), r.randint(128, 191)]),
+                        bytes([244, r.randint(128, 143),
+                               r.randint(128, 191), r.randint(128, 191)])])
+    else:
+        return Exception("gen_rand_utf8_char: len is NOT valid")
+    ret.decode(encoding="utf-8", errors="strict")  # check whether it is really UTF-8 string
+    return ret
+
+
+def gen_rand_utf8_string(len):
+    """ Returns randomly generated utf-8 string with given length as bytes.
+
+    Parameters:
+        len: int
+            number of utf-8 characters NOT bytes """
+    ret = b"".join(gen_rand_utf8_char(r.randint(1, 4)) for _ in range(len))
+    ret.decode(encoding="utf-8", errors="strict")  # check whether it is really UTF-8 string
+    return ret
